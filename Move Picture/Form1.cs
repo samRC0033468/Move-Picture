37a487407e1148c3c6b0c929084da501c3803200
david.petticrew <david.petticrew@CRO-24-AV>
2018-09-11 10:22:09 +0100

    Move to the right

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -16,5 +16,10 @@ namespace Move_Picture
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+        }
     }
 }
